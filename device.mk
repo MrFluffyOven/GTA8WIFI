f78389dcbd56f6283fae1f09477822a6b4d344ab
MrFluffyOven <137286588+MrFluffyOven@users.noreply.github.com>
2023-08-08 18:19:38 +0100

    Update device.mk

--- device.mk
+++ device.mk
@@ -24,11 +24,6 @@ PRODUCT_PACKAGES += \
 android.hardware.fastboot@1.0-impl-mock \
     fastbootd
 
-# fastbootd
-PRODUCT_PACKAGES += \
-    android.hardware.fastboot@1.0-impl-mock \
-    fastbootd
-
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
 PRODUCT_SHIPPING_API_LEVEL := 30
