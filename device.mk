6c4831ea2bc8a15b710d0f3ac14c42987ff5f9f0
MrFluffyOven <137286588+MrFluffyOven@users.noreply.github.com>
2024-02-10 09:33:17 +0000

    Update device.mk

--- device.mk
+++ device.mk
@@ -19,12 +19,6 @@ PRODUCT_PACKAGES += \
     bootctrl.$(PRODUCT_PLATFORM) \
     bootctrl.$(PRODUCT_PLATFORM).recovery \
 
-# vulkan
-PRODUCT_COPY_FILES += frameworks/native/data/etc/android.hardware.vulkan.version-1_1.xml:
-
-# Test Seperator
-$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.version-1_1.xml
-
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
