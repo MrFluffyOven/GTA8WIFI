4d8d3716cb50bf78910c357dc363dd3ffb76d9b7
MrFluffyOven <137286588+MrFluffyOven@users.noreply.github.com>
2024-03-14 12:35:00 +0000

    Update twrp_gta8wifi.mk

--- twrp_gta8wifi.mk
+++ twrp_gta8wifi.mk
@@ -17,7 +17,7 @@
 # Release name
 PRODUCT_RELEASE_NAME := gta8wifi
 
-#$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 #$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 #$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
