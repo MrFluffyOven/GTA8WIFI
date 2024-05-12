86a93d0a0667166499cef723c60d76c378149c5b
MrFluffyOven <137286588+MrFluffyOven@users.noreply.github.com>
2024-05-12 15:49:37 +0100

    remove keymaster 41

--- device.mk
+++ device.mk
@@ -37,11 +37,9 @@ PRODUCT_PACKAGES += \
 # Additional target Libraries
 TARGET_RECOVERY_DEVICE_MODULES += \
     libkeymaster4 \
-    libkeymaster41.so \
     libpuresoftkeymasterdevice
 RECOVERY_LIBRARY_SOURCE_FILES += \
     $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster4.so \
-    $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster41.so \
     $(TARGET_OUT_SHARED_LIBRARIES)/libpuresoftkeymasterdevice.so
 
 # Soong namespaces
