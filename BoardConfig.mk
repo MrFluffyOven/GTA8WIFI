d7462a9115a64c967532d2da73634444309771ab
MrFluffyOven <137286588+MrFluffyOven@users.noreply.github.com>
2024-05-13 11:07:12 +0100

    set permissive

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -65,7 +65,7 @@ TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 
 # Kernel
 BOARD_KERNEL_CMDLINE := console=ttyS1,115200n8
-BOARD_KERNEL_CMDLINE += androidboot.selinux=enforce androidboot.init_fatal_reboot_target=recovery
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive androidboot.init_fatal_reboot_target=recovery
 BOARD_KERNEL_IMAGE_NAME := Image.gz
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_BOOT_HEADER_VERSION := 2
