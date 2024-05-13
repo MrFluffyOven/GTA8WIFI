199920b7fead67664602e89f34f264033382444a
MrFluffyOven <137286588+MrFluffyOven@users.noreply.github.com>
2024-05-13 09:19:34 +0100

    remove dupe

--- device.mk
+++ device.mk
@@ -37,10 +37,8 @@ PRODUCT_PACKAGES += \
 # Additional target Libraries
 TARGET_RECOVERY_DEVICE_MODULES += \
     libkeymaster4 \
-    libpuresoftkeymasterdevice
 RECOVERY_LIBRARY_SOURCE_FILES += \
     $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster4.so \
-    $(TARGET_OUT_SHARED_LIBRARIES)/libpuresoftkeymasterdevice.so
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
